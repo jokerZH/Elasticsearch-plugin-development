da1b8ac7b9fb654d89e22a0d594c0bc4f3198ad7
Daniel Dai <335860828@qq.com>
2019-02-21 22:29:21 +0800

    持续更新

---
+++ example-plugin/src/main/java/org/elasticsearch/plugin/example/ExamplePlugin.java
@@ -0,0 +1,14 @@
+package org.elasticsearch.plugin.example;
+
+import org.elasticsearch.common.collect.Lists;
+import org.elasticsearch.common.inject.Module;
+import org.elasticsearch.plugins.AbstractPlugin;
+import java.util.Collection;
+public class ExamplePlugin extends AbstractPlugin {
+    public String name() { return "example-plugin"; }
+    public String description() { return "Example Plugin Description"; }
+    @Override public Collection<Class<? extends Module>> modules() {
+        Collection<Class<? extends Module>> modules = Lists.newArrayList();
+        modules.add(ExampleRestModule.class);
+        return modules;}
+}
---
+++ example-plugin/src/main/java/org/elasticsearch/plugin/example/ExampleRestModule.java
@@ -0,0 +1,7 @@
+package org.elasticsearch.plugin.example;
+import org.elasticsearch.common.inject.AbstractModule;
+public class ExampleRestModule extends AbstractModule {
+    @Override protected void configure() {
+        bind(HelloRestHandler.class).asEagerSingleton();
+    }
+}
